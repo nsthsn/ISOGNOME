bc4a634751c481ae818d9261aae5cf5130e31c04
doomcalf <alias.elias@gmail.com>
2021-03-01 09:49:46 -0600

    added base scripts, imported gamelogic extensions

---
+++ Assets/Scripts/3DPlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SRP: Changes player state based off input from outside controllers
+/// </summary>
+public class 3DPlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SRP : Accepts user input and commands PlayerController
+/// </summary>
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
